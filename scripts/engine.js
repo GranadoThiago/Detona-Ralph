1465e46bdddeea4ecc12cc51c36b5520c2cb309c
Capadocio <110613133+GranadoThiago@users.noreply.github.com>
2024-11-12 23:06:08 -0300

    update js

--- scripts/engine.js
+++ scripts/engine.js
@@ -5,17 +5,39 @@ const state = {
         timeLeft: document.querySelector("#time-left"),
         score: document.querySelector("#score"),
     },
-    values: {},
+    values: {
+        timerId: null,
+        gameVelocity: 1000,
+    },
 };
+
 /* Sorteando os quadrados aleatóriamente */
-function
+
+function randomSquare(){
+    state.view.squares.forEach((square)=> {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+}
+
+/* Função para mover o Enemy */
+
+function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
 
 /* Função para mudar o alvo */
 function addListenerHitBox() {
     state.view.squares.forEach((square)=> {});
 }
 
-function init()  {}
+function init() {
+    moveEnemy();
+}
 
 init();
 

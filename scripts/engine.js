784705c555daf611d70532169336f41ad5cb0ad9
Capadocio <110613133+GranadoThiago@users.noreply.github.com>
2024-11-12 21:26:32 -0300

    criação do js

---
+++ scripts/engine.js
@@ -0,0 +1,21 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {},
+};
+/* Sorteando os quadrados aleatóriamente */
+function
+
+/* Função para mudar o alvo */
+function addListenerHitBox() {
+    state.view.squares.forEach((square)=> {});
+}
+
+function init()  {}
+
+init();
+
